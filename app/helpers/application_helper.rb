8c2b0a98a680b1e6ad65f040e7f4adea742443bb
Sanad Bukiran <sanad.salem@gmail.com>
2016-11-20 09:19:16 +0300

    adding bootstrap gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(title = 'Untitled')
+    base_title = " | Sanad's Blog"
+    if title.empty?
+      title = 'Untitled'
+    end
+    return title + base_title
+  end
 end
